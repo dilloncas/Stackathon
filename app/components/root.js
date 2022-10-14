824a2622198653b931ece8bc5da70d1a22abc3d1
Travis Stratton <t.strat7@gmail.com>
2022-10-14 16:44:10 -0500

    Adding react-router-dom dependency

--- app/components/root.js
+++ app/components/root.js
@@ -3,9 +3,7 @@ import React from 'react'
 const Root = () => {
   return (
     <div>
-      <nav>
-        Goodie Bag
-      </nav>
+      <nav>Goodie Bag</nav>
       <main>
         <h1>Welcome to the Goodie Bag!</h1>
         <p>What a nice home page for your goodies!</p>
--- app/main.js
+++ app/main.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import store from './store'
 import Root from './components/root'
 
-ReactDOM.render(
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const root = createRoot(document.getElementById('main'))
+
+root.render(
   <Provider store={store}>
-    <Root />
-  </Provider>,
-  document.getElementById('main')
+    <Router>
+      <Root />
+    </Router>
+  </Provider>
 )
